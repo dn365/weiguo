2958d46551fcdbbd23b358c3e8345da21143df3f
dn365 <duning3650@gmail.com>
2014-04-15 11:27:11 +0800

    该cart session为cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,11 +26,20 @@ class ApplicationController < ActionController::Base
   end
 
 
+  # def current_cart
+  #   Cart.find(session[:cart_id])
+  # rescue ActiveRecord::RecordNotFound
+  #   cart = Cart.create
+  #   session[:cart_id] = cart.id
+  #   cart
+  # end
+
+
   def current_cart
-    Cart.find(session[:cart_id])
+    Cart.find(cookies.signed[:cart_id])
   rescue ActiveRecord::RecordNotFound
     cart = Cart.create
-    session[:cart_id] = cart.id
+    cookies.signed[:cart_id] = cart.id
     cart
   end
 
